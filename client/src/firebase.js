9a40735207205842bc95fe0cbcf932cf69bbaca6
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2024-01-12 15:36:50 +0100

    commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mernauth-e642e.firebaseapp.com",
+  projectId: "mernauth-e642e",
+  storageBucket: "mernauth-e642e.appspot.com",
+  messagingSenderId: "330161099346",
+  appId: "1:330161099346:web:8fabd76e325b0eb434458b"
 };
 
 // Initialize Firebase
